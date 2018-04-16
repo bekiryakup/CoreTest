acee2079a5015d478899eefd23bbb601077b9a31
bekiryakup <bekiryakup@gmail.com>
2018-04-16 19:00:06 +0300

    Lesson 2 Completed

--- mywebapp/Startup.cs
+++ mywebapp/Startup.cs
@@ -5,12 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace mywebapp
 {
     public class Startup
     {
+        public IConfiguration _config { get; set; }
+        public  Startup (IConfiguration config)
+        {
+            _config=config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,6 +27,7 @@ namespace mywebapp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
