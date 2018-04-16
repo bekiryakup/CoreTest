9565cd5fc75431026cae848c619e522a0ba95734
bekiryakup <bekiryakup@gmail.com>
2018-04-16 18:43:20 +0300

    First Commit

---
+++ Console App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+             Console.ReadLine();
+             Console.WriteLine("Hello World!");
+        }
+    }
+}
